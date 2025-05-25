d94ee121682c7d4fa0cd8d231fd98bda08558fe1
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-05-25 22:54:06 +0200

    UPDATED '15' with solution.

--- problems/15-choose-primes.js
+++ problems/15-choose-primes.js
@@ -20,10 +20,24 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 let isPrime = function(num) {
     // Your code here
+    let result = true;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            result = false;
+        }
+    }
+
+    return result;
 }
 
 let choosePrimes = function(nums) {
     // Your code here
+    return nums.filter(function(num) {
+        if(isPrime(num)) {
+            return num;
+        }
+    });
 }
 
 // alternative solution using Array.filter
@@ -37,4 +51,4 @@ try {
     module.exports = choosePrimes;
 } catch (e) {
     module.exports = null;
-}
+}
